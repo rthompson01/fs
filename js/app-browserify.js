78856f9f5254864cbbdca83a0fc2c8a732d626eb
Rey Thompson <rthompson1@gmail.com>
2015-06-11 12:42:02 -0500

    updated code. still in progress

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -2,25 +2,48 @@
 
 // es5 polyfills, powered by es5-shim
 require("es5-shim")
-// es6 polyfills, powered by babel
+   // es6 polyfills, powered by babel
 require("babel/register")
-
+var backbone = require('backbone')
 var Promise = require('es6-promise').Promise
-// just Node?
-// var fetch = require('node-fetch')
-// Browserify?
-// require('whatwg-fetch') //--> not a typo, don't store as a var
 
-// other stuff that we don't really use in our own code
-// var Pace = require("../bower_components/pace/pace.js")
 
-// require your own libraries, too!
-// var Router = require('./app.js')
+var Geo = new Promise((res, rej) => {
+   navigator.geolocation.getCurrentPosition((gpsData) => req({
+       lat: gpsData.coords.latitude,
+       lng: gpsData.coords.longitude
+   })((error) => reg(error.message)))
+})
 
-// window.addEventListener('load', app)
+var FourSquareModel = backbone.Model.extend({
+   initiialize: function() {
+       this.on('request', () => {
+           console.log('requesting data')
+       })
+       this.on('sync', () => {
+           console.log('request finished')
+       })
+       this.on('error', (...args) => {
+           console.error(args)
+       })
+   },
+})
 
-// function app() {
-    // start app
-    // new Router()
-// }
+var BrewSquareCollection = backbone.Collection.extend({
+   model: FourSquareModel,
+   id: "NLZAOKRUP1KFSRFHA1SQO3DH2214LAF0QV4UTEGMBHQG020Y",
+   secret: "UUCRXDQ2R1YITE04WPG0CAS40UBYREIAZTX0D0JZ0A2TEUX5",
+   query: 'breweries',
+   url: function() {
+       return `https://api.foursquare.com/v2/venues/search
+     ?client_id=${this.id}
+     &client_secret=${this.secret}
+     &v=20130815
+     &ll=40.7,-74
+     &query=${this.query}`
+   },
+   parse: (data) => data.response.venues
 
+})
+
+var brewdogs = new BrewSquareCollection();
